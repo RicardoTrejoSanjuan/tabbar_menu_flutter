4dcdf2ceabd2d93620089e54f4e3574315e07fcf
Ricardo Trejo Sanjuan <ricardo.trejo@stratplus.net>
2020-01-30 15:15:50 -0600

    first commit

--- lib/views/Home.dart
+++ lib/views/Home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:menu_tabbar/components/MenuTabBar.dart';
 
 class Home extends StatefulWidget {
 
@@ -12,26 +11,51 @@ class _Home extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: new SafeArea(child: new Stack(children: <Widget>[
-          new Center(child:
-            new Text("View", style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold))
-          ),
-          new MenuTabBar(
-            background: Colors.blue,
-            iconButtons: [
-              new IconButton(color: Colors.blue, icon: new Icon(Icons.home, size: 30), onPressed: (){}),
-              new IconButton(color: Colors.blue, icon: new Icon(Icons.search, size: 30), onPressed: (){}),
-              new IconButton(color: Colors.blue, icon: new Icon(Icons.map, size: 30), onPressed: (){}),
-              new IconButton(color: Colors.blue, icon: new Icon(Icons.favorite, size: 30), onPressed: (){}),
-            ],
-            child: new Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              new Container(child: new Text("Reminder", style: TextStyle(color: Colors.white, fontSize: 20)), margin: EdgeInsets.all(10)),
-              new Container(child: new Text("Camera", style: TextStyle(color: Colors.white, fontSize: 20)), margin: EdgeInsets.all(10)),
-              new Container(child: new Text("Attchment", style: TextStyle(color: Colors.white, fontSize: 20)), margin: EdgeInsets.all(10)),
-              new Container(child: new Text("Text Note", style: TextStyle(color: Colors.white, fontSize: 20)), margin: EdgeInsets.all(10))
-            ]),
+        appBar: AppBar(
+          backgroundColor: Colors.cyan,
+          title: Text('App'),
+        ),
+        body: new SafeArea(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Center(child:
+                new Text("View", style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold))
+              ),
+            ]
           )
-        ]))
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              title: Text('Business'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              title: Text('School'),
+            ),
+          ],
+        )
+        // bottomNavigationBar: new MenuTabBar(
+        //     background: Colors.blue,
+        //     iconButtons: [
+        //       new IconButton(color: Colors.blue, icon: new Icon(Icons.home, size: 30), onPressed: (){}),
+        //       new IconButton(color: Colors.blue, icon: new Icon(Icons.search, size: 30), onPressed: (){}),
+        //       new IconButton(color: Colors.blue, icon: new Icon(Icons.map, size: 30), onPressed: (){}),
+        //       new IconButton(color: Colors.blue, icon: new Icon(Icons.favorite, size: 30), onPressed: (){}),
+        //     ],
+        //     child: new Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+        //       new Container(child: new Text("Reminder", style: TextStyle(color: Colors.white, fontSize: 20)), margin: EdgeInsets.all(10)),
+        //       new Container(child: new Text("Camera", style: TextStyle(color: Colors.white, fontSize: 20)), margin: EdgeInsets.all(10)),
+        //       new Container(child: new Text("Attchment", style: TextStyle(color: Colors.white, fontSize: 20)), margin: EdgeInsets.all(10)),
+        //       new Container(child: new Text("Text Note", style: TextStyle(color: Colors.white, fontSize: 20)), margin: EdgeInsets.all(10))
+        //     ]),
+        // )
     );
   }
 }
