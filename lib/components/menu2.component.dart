1122496ab720ada99f3e9f9769619edfafd820e2
Ricardo Trejo Sanjuan <ricardo.trejo@stratplus.net>
2020-02-04 10:35:32 -0600

    alerta menu final

--- lib/components/menu2.component.dart
+++ lib/components/menu2.component.dart
@@ -12,27 +12,83 @@ Future<void> showRevealDialog(BuildContext context) async {
     pageBuilder: (context, anim1, anim2) {
       return Align(
         alignment: Alignment.bottomCenter,
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          color: Colors.orange,
-          child: RaisedButton(
-            color: Colors.blue,
-            child: Text('data'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
+        child: Center(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            color: Colors.orange,
+            child: new ListView(
+              // shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+              children: <Widget>[
+                // Center(
+                  new FlatButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: new Text("Facebook"),
+                  ),
+                // ),
+                // new Padding(padding: new EdgeInsets.all(5.00)),
+                // new RaisedButton(
+                //   onPressed: () {
+                //     Navigator.of(context).pop();
+                //   },
+                //   child: new Text("Google"),
+                // )
+              ],
+      //   ListTile(title: Text('item1')),
+      //   Divider(),
+      //   ListTile(title: Text('item1')),
+      //   Divider(),
+      //   ListTile(title: Text('item1')),
+      //   Divider(),
+      // ]
+              // _listaItems(context)
+              // <Widget>[
+                // new RaisedButton(
+                //   onPressed: () {
+                //     Navigator.of(context).pop();
+                //   },
+                //   child: new Text("Facebook"),
+                // ),
+                // new Padding(padding: new EdgeInsets.all(5.00)),
+                // new RaisedButton(
+                //   onPressed: () {
+                //     Navigator.of(context).pop();
+                //   },
+                //   child: new Text("Google"),
+                // )
+              // ],
+            ),
+            // _lista(context),
+            // Column(
+            //   // mainAxisSize: MainAxisSize.min,
+            //   children: <Widget>[
+            //     Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore'),
+            //     FlutterLogo(size: 100.0)
+            //   ],
+            // ),
+
+            // RaisedButton(
+            //   color: Colors.blue,
+            //   child: Text('data'),
+            //   onPressed: () {
+            //     Navigator.of(context).pop();
+            //   },
+            // ),
+
+            // child: Padding(
+            //   padding: const EdgeInsets.all(12.0),
+            //   child: FlutterLogo(size: 100.0)
+            //   // Image.asset('assets/klimt.png'),
+            // ),
+            // margin: EdgeInsets.only(top: 50, left: 12, right: 12, bottom: 0),
+            // decoration: BoxDecoration(
+            //   color: Colors.white,
+            //   borderRadius: BorderRadius.circular(5),
+            // ),
           ),
-          // child: Padding(
-          //   padding: const EdgeInsets.all(12.0),
-          //   child: FlutterLogo(size: 100.0)
-          //   // Image.asset('assets/klimt.png'),
-          // ),
-          // margin: EdgeInsets.only(top: 50, left: 12, right: 12, bottom: 0),
-          // decoration: BoxDecoration(
-          //   color: Colors.white,
-          //   borderRadius: BorderRadius.circular(5),
-          // ),
         ),
       );
       // Container(
@@ -56,4 +112,106 @@ Future<void> showRevealDialog(BuildContext context) async {
       );
     },
   );
+
+  Widget optionOne = SimpleDialogOption(
+    child: const Text('horse'),
+    onPressed: () {
+      print('horse');
+      Navigator.of(context).pop();
+    },
+  );
+  Widget optionTwo = SimpleDialogOption(
+    child: const Text('cow'),
+    onPressed: () {
+      print('cow');
+      Navigator.of(context).pop();
+    },
+  );
+
+
+  Widget _lista(BuildContext context) {
+
+    // Opcion Poco convencional
+    // menuProvider.cargarData()
+    // .then((res) {
+    //   print('Lista');
+    //   print(res);
+    // });
+
+    return ListView(
+      // children: _listaItems(context),
+    );
+
+    // return FutureBuilder(
+    //   future: menuProvider.cargarData(),
+    //   initialData: [],
+    //   builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+
+    //     // print('builder');
+    //     // print(snapshot.data);
+    //     return ListView(
+    //       children: _listaItems(snapshot.data, context),
+    //     );
+    //   },
+    // );
+  }
+
+
+
+  // List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
+  List<Widget> _listaItems(BuildContext context) {
+
+    // ---  Opcion 2 para el menu ----
+
+    // final List<Widget> lista = [];
+
+    // data.forEach((item) {
+    //   final widgetTemp = ListTile(
+    //     title: Text(item['texto']),
+    //     leading: getIcon(item['icon']),
+    //     trailing: Icon(Icons.keyboard_arrow_right),
+    //     onTap: () {
+
+    //       Navigator.pushNamed(context, item['ruta']);
+
+    //       // Esta opcion funciona pero es de forma estatica
+    //       // final route = MaterialPageRoute(
+    //       //   builder: (context) => AlertPage()
+    //       // );
+    //       // Navigator.push(context, route);
+    //     },
+    //   );
+
+    //   lista..add(widgetTemp)
+    //        ..add(Divider());
+    // });
+
+    // --  End opcion 2 para el menu ---
+
+    // ---  Opcion 1 para el menu ----
+    // final List<Widget> lista = data.map((item) {
+    //   return Column(
+    //     children: <Widget>[
+    //       ListTile(
+    //         title: Text(item['texto']),
+    //         leading: Icon(Icons.mood, color: Colors.blue),
+    //         trailing: Icon(Icons.keyboard_arrow_right),
+    //         onTap: () {},
+    //       ),
+    //       Divider(),
+    //     ],
+    //   );
+    // }).toList();
+
+    // return lista;
+
+    return [
+      ListTile(title: Text('item1')),
+      Divider(),
+      ListTile(title: Text('item1')),
+      Divider(),
+      ListTile(title: Text('item1')),
+      Divider(),
+    ];
+  }
 }
--- lib/components/tabbar.component.dart
+++ lib/components/tabbar.component.dart
@@ -21,9 +21,6 @@ class _TabBarWidgetState extends State<TabBarWidget> {
       mostrarAlerta(context);
       break;
     case 1:
-      showRevealDialog(context);
-      break;
-    case 2:
       showNewDialog(
         context: context,
         builder: (BuildContext context) {
@@ -32,6 +29,9 @@ class _TabBarWidgetState extends State<TabBarWidget> {
           );
         },
       );
+      break;
+    case 2:
+      showRevealDialog(context);
       break;
     case 3:
       break;
@@ -69,171 +69,4 @@ class _TabBarWidgetState extends State<TabBarWidget> {
     });
     return lista;
   }
-
-  // _mostrarAlerta(BuildContext context) {
-  //   showGeneralDialog(
-  //     context: context,
-  //     pageBuilder: (BuildContext buildContext, Animation<double> animation, Animation<double> secondaryAnimation) {
-  //       // return Center(
-  //       //   child: Container(
-  //       //     width: MediaQuery.of(context).size.width,
-  //       //     height: MediaQuery.of(context).size.height,
-  //       //     color: Colors.orange,
-  //           // child: RaisedButton(
-  //           //   child: Text('data'),
-  //           //   onPressed: () {
-  //           //     Navigator.of(context).pop();
-  //           //   },
-  //           // ),
-  //       //   ),
-  //       // );
-  //       return Wrap(
-  //         children: <Widget>[
-  //           Container(
-  //             width: MediaQuery.of(context).size.width,
-  //             height: MediaQuery.of(context).size.height,
-  //             color: Colors.orange,
-  //             child: RaisedButton(
-  //               // color: Colors.blue,
-  //               child: Text('data'),
-  //               onPressed: () {
-  //                 Navigator.of(context).pop();
-  //               },
-  //             ),
-  //           ),
-  //         ],
-  //       );
-  //     },
-  //     barrierDismissible: true,
-  //     barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
-  //     barrierColor: Colors.green,
-  //     transitionDuration: const Duration(milliseconds: 500),
-  //     transitionBuilder: _buildNewTransition
-  //   );
-  // }
-
-  // Widget _buildNewTransition(
-  //   BuildContext context,
-  //   Animation<double> animation,
-  //   Animation<double> secondaryAnimation,
-  //   Widget child,
-  // ) {
-  //   return ScaleTransition(
-  //     scale: CurvedAnimation(
-  //       parent: animation,
-  //       curve: Curves.easeIn,
-  //       reverseCurve: Curves.easeIn,
-  //     ),
-  //     child: child,
-  //   );
-  // }
-
-// Future<T> showNewDialog<T>({
-//   @required BuildContext context,
-//   bool barrierDismissible = true,
-//   Widget child,
-//   WidgetBuilder builder,
-// }) {
-//   assert(child == null || builder == null);
-//   assert(debugCheckHasMaterialLocalizations(context));
-
-//   final ThemeData theme = Theme.of(context, shadowThemeOnly: true);
-//   return showGeneralDialog(
-//     context: context,
-//     pageBuilder: (BuildContext buildContext, Animation<double> animation, Animation<double> secondaryAnimation) {
-//       // final Widget pageChild = child ?? Builder(builder: builder);
-//       return Wrap(
-//         children: <Widget>[
-//           Container(
-//             width: 200.0,
-//             height: 200.0,
-//             // color: Colors.orange,
-//           ),
-//         ],
-//       );
-//       // return Builder(builder: (BuildContext context) {
-//       //   return theme != null ? Theme(data: theme, child: pageChild) : pageChild;
-//       // });
-//     },
-//     barrierDismissible: barrierDismissible,
-//     barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
-//     barrierColor: Colors.black54,
-//     // barrierColor: Colors.yellow[700],
-//     transitionDuration: const Duration(milliseconds: 400),
-//     transitionBuilder: _buildNewTransition,
-//   );
-// }
-
-// Widget _buildNewTransition(
-//   BuildContext context,
-//   Animation<double> animation,
-//   Animation<double> secondaryAnimation,
-//   Widget child,
-// ) {
-//   return ScaleTransition(
-//     scale: CurvedAnimation(
-//       parent: animation,
-//       curve: Curves.bounceIn,
-//       reverseCurve: Curves.bounceIn,
-//     ),
-//     child: child,
-//   );
-// }
-
-
-// Future<void> showRevealDialog(BuildContext context) async {
-//     showGeneralDialog(
-//       barrierLabel: "Label",
-//       barrierDismissible: true,
-//       barrierColor: Colors.black.withOpacity(0.5),
-//       transitionDuration: Duration(milliseconds: 500),
-//       context: context,
-//       pageBuilder: (context, anim1, anim2) {
-//         return Align(
-//           alignment: Alignment.bottomCenter,
-//           child: Container(
-//             width: MediaQuery.of(context).size.width,
-//             height: MediaQuery.of(context).size.height,
-//             color: Colors.orange,
-//             child: RaisedButton(
-//               color: Colors.blue,
-//               child: Text('data'),
-//               onPressed: () {
-//                 Navigator.of(context).pop();
-//               },
-//             ),
-//             // child: Padding(
-//             //   padding: const EdgeInsets.all(12.0),
-//             //   child: FlutterLogo(size: 100.0)
-//             //   // Image.asset('assets/klimt.png'),
-//             // ),
-//             // margin: EdgeInsets.only(top: 50, left: 12, right: 12, bottom: 0),
-//             // decoration: BoxDecoration(
-//             //   color: Colors.white,
-//             //   borderRadius: BorderRadius.circular(5),
-//             // ),
-//           ),
-//         );
-//         // Container(
-//         //       width: MediaQuery.of(context).size.width,
-//         //       height: MediaQuery.of(context).size.height,
-//         //       color: Colors.orange,
-//         //       child: RaisedButton(
-//         //         // color: Colors.blue,
-//         //         child: Text('data'),
-//         //         onPressed: () {
-//         //           Navigator.of(context).pop();
-//         //         },
-//         //       ),
-//         //     ),
-//       },
-//       transitionBuilder: (context, anim1, anim2, child) {
-//         return CircularRevealAnimation(
-//           child: child,
-//           animation: anim1,
-//           centerAlignment: Alignment.bottomCenter,
-//         );
-//       },
-//     );
-//   }
 }
